9a01b42f09c226d8ccee257938ccd6288a89f82b
Tim Li <tianleli@berkeley.edu>
2023-11-07 22:02:30 -0800

    change name

--- judge_any.py
+++ judge_any.py
@@ -125,77 +125,10 @@ def judgment(**args):
         f.write(json.dumps(output, ensure_ascii=False) + "\n")
 
 
-def turn_by_turn_judgment(**args):
-    configs = args["configs"]
-    reference = args["reference"]
-
-    model = configs["judge_model"]
-    judgments = []
-    convs = []
-
-    for i, turn in enumerate(args["iterable"]):
-        conv = get_conversation_template(model)
-        conv.set_system_message(configs["system_prompt"])
-
-        prompt_args = {}
-        if reference:
-            assert reference[i]
-            prompt_args[f"ref_answer_{1}"] = reference[i]
-
-        prompt_args[f"question_{1}"] = turn
-        user_prompt = configs["prompt_template"].format(**prompt_args)
-
-        # add few shot prompts
-        if configs["few_shot"]:
-            for i, shot in enumerate(configs["few_shot"]):
-                conv.append_message(conv.roles[i % 2], shot)
-
-        conv.append_message(conv.roles[0], user_prompt)
-        conv.append_message(conv.roles[1], None)
-
-        output = get_answer(
-            model,
-            conv,
-            configs["temperature"],
-            configs["max_tokens"],
-            configs["endpoint_list"],
-        )
-
-        judgments.append(output)
-        convs.append(conv.messages)
-
-    with open(args["output_file"], "a") as f:
-        output = {}
-        output["question_id"] = args["question_id"]
-        if args["answer_id"]:
-            output["answer_id"] = args["answer_id"]
-        else:
-            output["answer_id"] = shortuuid.uuid()
-        if args["model_id"]:
-            output["model_id"] = args["model_id"]
-        output["judge_answer"] = judgments
-        output["prompt"] = convs
-
-        f.write(json.dumps(output, ensure_ascii=False) + "\n")
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config-file", type=str, required=True)
+    parser.add_argument("--config-file", type=str, default="arena-bench-v1")
     parser.add_argument("--bench-name", type=str, required=True)
-    parser.add_argument(
-        "--mode",
-        type=str,
-        default="all",
-        choices=["all", "question", "answer"],
-        help=(
-            "Judge mode."
-            "`all` judge the entire conversation."
-            "`question` judge prompts only turn-by-turn"
-            "`answer` judge model answers only turn-by-turn"
-        ),
-        required=True,
-    )
     parser.add_argument(
         "--model",
         type=str,
@@ -220,47 +153,7 @@ if __name__ == "__main__":
 
     if args.model:
         model_answers = {args.model:model_answers[args.model]}
-
-    # if mode isn't all, then need to create a iterable list for turn-by-turn judgment
-    if args.mode == "question":
-        iterables = []
-        for prompt in questions:
-            obj = {}
-            obj["iterables"] = prompt["turns"]
-            if "question_id" in prompt:
-                obj["question_id"] = prompt["question_id"]
-            else:
-                obj["question_id"] = None
-            if "answer_id" in prompt:
-                obj["answer_id"] = prompt["answer_id"]
-            else:
-                obj["answer_id"] = None
-            if "model_id" in prompt:
-                obj["model_id"] = prompt["model_id"]
-            else:
-                obj["model_id"] = None
-            iterables.append(obj)
-    elif args.mode == "answer":
-        iterables = []
-        for model in model_answers.keys():
-            for i in model_answers[model].keys():
-                prompt = model_answers[model][i]
-                obj = {}
-                obj["iterables"] = prompt["choices"][0]["turns"]
-                if "question_id" in prompt:
-                    obj["question_id"] = prompt["question_id"]
-                else:
-                    obj["question_id"] = None
-                if "answer_id" in prompt:
-                    obj["answer_id"] = prompt["answer_id"]
-                else:
-                    obj["answer_id"] = None
-                if "model_id" in prompt:
-                    obj["model_id"] = prompt["model_id"]
-                else:
-                    obj["model_id"] = None
-                iterables.append(obj)
-
+        
     ref_answers = None
     if configs["reference"]:
         ref_answers = load_model_answers(ref_answer_dir)
@@ -280,43 +173,21 @@ if __name__ == "__main__":
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=args.parallel) as executor:
         futures = []
-        if args.mode == "all":
-            for model in model_answers.keys():
-                for question in questions:
-                    kwargs = {}
-                    kwargs["question"] = question
-                    kwargs["answer"] = model_answers[model][question["question_id"]]
-                    if ref_answers:
-                        kwargs["reference"] = ref_answers[question["question_id"]]
-                    else:
-                        kwargs["reference"] = None
-
-                    kwargs["configs"] = configs
-                    kwargs["output_file"] = output_file
-                    future = executor.submit(judgment, **kwargs)
-                    futures.append(future)
-            for future in tqdm(
-                concurrent.futures.as_completed(futures), total=len(futures)
-            ):
-                future.result()
-        else:
-            for iter in iterables:
+        for model in model_answers.keys():
+            for question in questions:
                 kwargs = {}
-                kwargs["iterable"] = iter["iterables"]
-                kwargs["question_id"] = iter["question_id"]
-                kwargs["answer_id"] = iter["answer_id"]
-                kwargs["model_id"] = iter["model_id"]
-
+                kwargs["question"] = question
+                kwargs["answer"] = model_answers[model][question["question_id"]]
                 if ref_answers:
-                    kwargs["reference"] = ref_answers[iter["question_id"]]
+                    kwargs["reference"] = ref_answers[question["question_id"]]
                 else:
                     kwargs["reference"] = None
 
                 kwargs["configs"] = configs
                 kwargs["output_file"] = output_file
-                future = executor.submit(turn_by_turn_judgment, **kwargs)
+                future = executor.submit(judgment, **kwargs)
                 futures.append(future)
-            for future in tqdm(
-                concurrent.futures.as_completed(futures), total=len(futures)
-            ):
-                future.result()
+        for future in tqdm(
+            concurrent.futures.as_completed(futures), total=len(futures)
+        ):
+            future.result()
