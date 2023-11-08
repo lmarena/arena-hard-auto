57f2323ef1555d7f5d21e3000bcf04848f465e17
Tim Li <tianleli@berkeley.edu>
2023-11-07 23:33:06 -0800

    update show_result

--- show_result.py
+++ show_result.py
@@ -2,32 +2,19 @@ import pandas as pd
 import argparse
 import os
 import re
+import math
+import numpy as np
+from collections import OrderedDict
 from glob import glob
 
 pattern = re.compile(r"\[\[(\d*?)\]\]")
 
 def evaluate(df):
-    model_names = df["model"].unique()
-    model_scores = {}
-    for model_name in model_names:
-        scores = []
-        for i, row in df.iterrows():
-            if row["model"] != model_name:
-                continue
-            matches = pattern.findall(row["judgment"])
-            # remove empty string
-            matches = [m for m in matches if m != ""]
-            if len(matches) == 0:
-                print("WARNING: no score pattern matched... skipping "
-                      f"(model={row['model']}, qid={row['question_id']})")
-            elif len(matches) == 1:
-                scores.append(int(matches[0]))
-            else:
-                print("WARNING: more than one score pattern matched")
-        model_scores[model_name] = scores
-
-    for model_name, scores in model_scores.items():
-        print(f"Model: {model_name} | Score: {sum(scores) / len(scores)} | # Valid-score: {len(scores)}")
+    scores = []
+    for i, row in df.iterrows():
+        if not math.isnan(row["score"]):
+            scores.append(int(row["score"]))
+    return scores
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -35,5 +22,17 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print(args)
 
+    model_scores = {}
     for judgment_file in glob(os.path.join("data", args.bench_name, "model_judgment", "*.jsonl")):
-        evaluate(pd.read_json(judgment_file, lines=True))
+        df = pd.read_json(judgment_file, lines=True)
+        assert len(df["model"].unique()) == 1
+        model_scores[df.iloc[0]["model"]] = evaluate(df)
+
+    keys = list(model_scores.keys())
+    values = list(model_scores.values())
+    value_sums = [-1 * sum(v) / len(v) for v in values]
+    sorted_value_index = np.argsort(value_sums)
+    sorted_dict = {keys[i]: values[i] for i in sorted_value_index}
+
+    for model_name, scores in sorted_dict.items():
+        print(f"Model: {model_name} | Score: {sum(scores) / len(scores)} | # Valid-score: {len(scores)}")
