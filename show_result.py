d48085bb6ef832795af2de718750fd679bd46423
Wei-Lin Chiang <weichiang@berkeley.edu>
2024-04-01 05:59:17 +0000

    update

--- show_result.py
+++ show_result.py
@@ -90,14 +90,14 @@ if __name__ == "__main__":
         lengths.append(int(length))
         
     leaderboard = pd.DataFrame()
-    leaderboard["models"] = models
-    leaderboard["scores"] = [s["rate"] for s in scores]
+    leaderboard["model"] = models
+    leaderboard["winrate"] = [s["rate"] for s in scores]
     leaderboard["stats"] = scores
     leaderboard["token_len"] = lengths
-    leaderboard = leaderboard.sort_values(by="scores", ascending=False)
+    leaderboard = leaderboard.sort_values(by="winrate", ascending=False)
     for i, row in leaderboard.iterrows():
         if args.full_stats:
-            print(f"{row['models'][:20] : <20} | win-rate: {row['scores'] : ^5} | vaild-score: {row['stats']['valid'] : ^3} | big-win: {row['stats']['E'] : ^2} | small-win: {row['stats']['D']} | tie: {row['stats']['C'] : ^2} | big-loss: {row['stats']['A'] : ^2} | small-loss: {row['stats']['B']}")
+            print(f"{row['model'][:20] : <20} | win-rate: {row['winrate'] : ^5} | vaild-score: {row['stats']['valid'] : ^3} | big-win: {row['stats']['E'] : ^2} | small-win: {row['stats']['D']} | tie: {row['stats']['C'] : ^2} | big-loss: {row['stats']['A'] : ^2} | small-loss: {row['stats']['B']}")
         else:
-            print(f"{row['models'] : <30} | win-rate: {row['scores'] : ^5} | average #tokens: {row['token_len']}")
-        # print(f"[{row['models']}, {row['scores']}: {row['token_len']}]")
+            print(f"{row['model'] : <30} | win-rate: {row['winrate'] : ^5} | average #tokens: {row['token_len']}")
+        # print(f"[{row['model']}, {row['scores']}: {row['token_len']}]")
