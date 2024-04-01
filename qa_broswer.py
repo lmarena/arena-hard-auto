96148526de2031487c2b413bb1583fa4493c85f9
Tim Li <tianleli@berkeley.edu>
2024-03-31 17:41:17 -0700

    complete UI broswer

--- qa_broswer.py
+++ qa_broswer.py
@@ -27,11 +27,8 @@ model_judgments_math_pairwise = {}
 question_selector_map = {}
 category_selector_map = defaultdict(list)
 
-model_reference_answers = []
-
-
 def display_question(category_selector, request: gr.Request):
-    choices = category_selector_map['arena-bench-v1']
+    choices = category_selector_map['arena-hard-v0.1']
     return gr.Dropdown.update(
         value=choices[0],
         choices=choices,
@@ -53,12 +50,16 @@ def display_pairwise_answer(
         ans3 = model_judgments_normal_single
 
     chat_mds = pairwise_to_gradio_chat_mds(q, ans1, ans2, ans_base=ans3)
+    chat_mds[1] = "##### Assistant A: " + chat_mds[1]
+    chat_mds[2] = "##### Assistant B: " + chat_mds[2]
+
     gamekey = (qid, model_selector1, model_selector2)
 
     judgment_dict = model_judgments_math_pairwise[qid]
 
     explanations = get_pairwise_judge_explanation(gamekey, judgment_dict)
-    return chat_mds + explanations
+    chat_mds_2 = chat_mds[:1] + chat_mds[:-3:-1]
+    return chat_mds + [explanations[0]] + chat_mds_2 + [explanations[1]]
 
 
 def display_single_answer(question_selector, model_selector1, request: gr.Request):
@@ -95,54 +96,37 @@ def pairwise_to_gradio_chat_mds(question, ans_a, ans_b, ans_base=None, turn=None
     end = len(question["turns"]) if turn is None else turn + 1
     size = end * 3
 
-    mds = ["" for i in range(size + len(model_reference_answers))]
+    mds = ["" for i in range(size)]
     for i in range(end):
         base = i * 3
         if i == 0:
-            mds[base + 0] = "##### User\n" + question["turns"][i]
+            mds[base + 0] = "##### User\n" + question["turns"][i]["content"]
         else:
-            mds[base + 0] = "##### User's follow-up question \n" + question["turns"][i]
-        mds[base + 1] = "##### Assistant A\n" + post_process_answer(
-            ans_a["choices"][0]["turns"][i].strip()
+            mds[base + 0] = "##### User's follow-up question \n" + question["turns"][i]["content"]
+        mds[base + 1] = f"{ans_a['model_id']}\n" + post_process_answer(
+            ans_a["choices"][0]["turns"][i]["content"].strip()
         )
-        mds[base + 2] = "##### Assistant B\n" + post_process_answer(
-            ans_b["choices"][0]["turns"][i].strip()
+        mds[base + 2] = f"{ans_b['model_id']}\n" + post_process_answer(
+            ans_b["choices"][0]["turns"][i]["content"].strip()
         )
 
-    if ans_base:
-        mds += [""]
-        mds[size] = f"##### Baseline\n" + post_process_answer(
-            ans_base["choices"][0]["turns"][0].strip()
-        )
-        size += 1
-
-    for i, model_references in enumerate(model_reference_answers):
-        model, references = model_references
-        ref = references[question["question_id"]]
-
-        mds[size + i] = f"##### Reference Solution: {model}\n{ref}"
     return mds
 
 
 def single_to_gradio_chat_mds(question, ans, turn=None):
     end = len(question["turns"]) if turn is None else turn + 1
 
-    mds = ["" for i in range(2 + len(model_reference_answers))]
+    mds = ["" for i in range(2)]
     for i in range(end):
         base = i * 2
         if i == 0:
-            mds[base + 0] = "##### User\n" + question["turns"][i]
+            mds[base + 0] = "##### User\n" + question["turns"][i]["content"]
         else:
-            mds[base + 0] = "##### User's follow-up question \n" + question["turns"][i]
+            mds[base + 0] = "##### User's follow-up question \n" + question["turns"][i]["content"]
         mds[base + 1] = "##### Assistant A\n" + post_process_answer(
-            ans["choices"][0]["turns"][i].strip()
+            ans["choices"][0]["turns"][i]["content"].strip()
         )
 
-    for i, model_references in enumerate(model_reference_answers):
-        model, references = model_references
-        ref = references[question["question_id"]]
-
-        mds[2 + i] = f"##### Reference Solution: {model}\n{ref}"
     return mds
 
 
@@ -151,7 +135,7 @@ def build_question_selector_map():
 
     # Build question selector map
     for i, q in enumerate(questions):
-        preview = f"{i+1}: " + q["turns"][0][:128] + "..."
+        preview = f"{i+1}: " + q["turns"][0]["content"][:128] + "..."
         question_selector_map[preview] = q
         category_selector_map[q["category"]].append(preview)
 
@@ -183,65 +167,85 @@ def build_pairwise_browser_tab():
         for i in range(num_sides):
             with gr.Column():
                 if i == 0:
-                    value = "Starling-LM-7B-alpha"
-                    # value = "gpt-3.5-turbo"
+                    model_selectors[i] = gr.Dropdown(
+                        choices=["gpt-4-0314"],
+                        value="gpt-4-0314",
+                        label=f"Model {side_names[i]}",
+                        container=False,
+                    )
                 else:
-                    value = "vicuna-33b-v1.3"
-                    # value = "gpt-3.5-turbo"
-                model_selectors[i] = gr.Dropdown(
-                    choices=models,
-                    value=value,
-                    label=f"Model {side_names[i]}",
-                    container=False,
-                )
+                    model_selectors[i] = gr.Dropdown(
+                        choices=models,
+                        value="gpt-3.5-turbo-0613",
+                        label=f"Model {side_names[i]}",
+                        container=False,
+                    )
 
-    # Conversation
     chat_mds = []
-    for i in range(num_turns):
-        chat_mds.append(gr.Markdown(elem_id=f"user_question_{i+1}"))
-        with gr.Row():
-            for j in range(num_sides):
+
+    with gr.Tabs() as tabs:
+        with gr.Tab("Game 1", id=0):
+            # Conversation
+            for i in range(num_turns):
+                chat_mds.append(gr.Markdown(elem_id=f"user_question_{i+1}"))
+                with gr.Row():
+                    for j in range(num_sides):
+                        with gr.Column(scale=100):
+                            chat_mds.append(gr.Markdown())
+
+                        if j == 0:
+                            with gr.Column(scale=1, min_width=8):
+                                gr.Markdown()
+
+            # if baseline_model:
+            #     baseline = gr.Markdown()
+            #     chat_mds.append(baseline)
+            
+            gr.Markdown("## Model Judgment Comparison \n")
+
+            with gr.Row():
                 with gr.Column(scale=100):
-                    chat_mds.append(gr.Markdown())
-
-                if j == 0:
+                    chat_mds.append(gr.Markdown(elem_id="model_explanation"))
                     with gr.Column(scale=1, min_width=8):
                         gr.Markdown()
+        with gr.Tab("Game 2", id=1):
+            # Conversation
+            for i in range(num_turns):
+                chat_mds.append(gr.Markdown(elem_id=f"user_question_{i+1}"))
+                with gr.Row():
+                    for j in range(num_sides):
+                        with gr.Column(scale=100):
+                            chat_mds.append(gr.Markdown())
 
-    if baseline_model:
-        baseline = gr.Markdown()
-        chat_mds.append(baseline)
-    
-    gr.Markdown("## Model Judgment Comparison \n")
+                        if j == 0:
+                            with gr.Column(scale=1, min_width=8):
+                                gr.Markdown()
 
-    model_explanations = []
-    with gr.Row():
-        for j in range(num_sides):
-            with gr.Column(scale=100):
-                model_explanations.append(gr.Markdown(elem_id="model_explanation"))
+            # if baseline_model:
+            #     baseline = gr.Markdown()
+            #     chat_mds.append(baseline)
+            
+            gr.Markdown("## Model Judgment Comparison \n")
 
-            if j == 0:
+            with gr.Row():
+                with gr.Column(scale=100):
+                    chat_mds.append(gr.Markdown(elem_id="model_explanation"))
                     with gr.Column(scale=1, min_width=8):
                         gr.Markdown()
-
-    for i in range(len(model_reference_answers)):
-        reference = gr.Markdown(elem_id=f"reference_{i+1}")
-        chat_mds.append(reference)
 
     # Callbacks
     category_selector.change(display_question, [category_selector], [question_selector])
     question_selector.change(
         display_pairwise_answer,
         [question_selector] + model_selectors,
-        chat_mds + model_explanations,
+        chat_mds,
     )
 
-    for i in range(num_sides):
-        model_selectors[i].change(
-            display_pairwise_answer,
-            [question_selector] + model_selectors,
-            chat_mds + model_explanations,
-        )
+    model_selectors[1].change(
+        display_pairwise_answer,
+        [question_selector] + model_selectors,
+        chat_mds,
+    )
 
     return (category_selector,)
 
@@ -261,7 +265,7 @@ def build_single_answer_browser_tab():
     with gr.Row():
         with gr.Column(scale=1, min_width=200):
             category_selector = gr.Dropdown(
-                choices=category_selector_choices, value='arena-bench-v1', label="Category", container=False
+                choices=category_selector_choices, value='arena-hard-v0.1', label="Category", container=False
             )
         with gr.Column(scale=100):
             question_selector = gr.Dropdown(
@@ -291,10 +295,6 @@ def build_single_answer_browser_tab():
                 if j == 0:
                     with gr.Column(scale=1, min_width=8):
                         gr.Markdown()
-
-    for i in range(len(model_reference_answers)):
-        reference = gr.Markdown(elem_id=f"reference_{i+1}")
-        chat_mds.append(reference)
 
     model_explanation = gr.Markdown(elem_id="model_explanation")
 
@@ -339,14 +339,14 @@ def build_demo():
     build_question_selector_map()
 
     with gr.Blocks(
-        title="Arena-Bench Browser",
+        title="Arena Hard Browser",
         theme=gr.themes.Base(text_size=gr.themes.sizes.text_lg),
         css=block_css,
     ) as demo:
         gr.Markdown(
             """
-# Arena-Bench-V1
-The code to generate answers and judgments is at [arena-bench](https://github.com/lm-sys/arena-bench).
+# Arena Hard v0.1
+The code to generate answers and judgments is at [arena-hard](https://github.com/lm-sys/arena-hard).
 """
         )
         with gr.Tab("Pairwise Comparison"):
@@ -356,18 +356,6 @@ The code to generate answers and judgments is at [arena-bench](https://github.co
         demo.load(load_demo, [], [category_selector2])
 
     return demo
-
-
-def load_reference(directory, ref_models):
-    all_references = []
-    for model in ref_models:
-        model_references = {}
-        with open(os.path.join(directory, f"{model}.jsonl")) as fin:
-                for line in fin:
-                    line = json.loads(line)
-                    model_references[line["question_id"]] = line["choices"][0]["turns"][0]
-        all_references.append((model, model_references))
-    return all_references
 
 
 # def load_pairwise_model_judgments(dir: str):
@@ -447,14 +435,13 @@ def load_single_model_judgments(dir: str):
 def get_pairwise_judge_explanation(gamekey, judgment_dict):
     """Get model judge explanation."""
     try:
-        qid, model_1, model_2 = gamekey
+        _, _, model_2 = gamekey
         
-        g1_judgment, g2_judgment = judgment_dict[model_1], judgment_dict[model_2]
+        g1_judgment = judgment_dict[model_2]
 
         # g1_judgment = g1_judgment[:-6] + f"<mark><span style='color:black'>{g1_judgment[-6:]}</span></mark>"
         # g2_judgment = g2_judgment[:-6] + f"<mark><span style='color:black'>{g2_judgment[-6:]}</span></mark>"
-        return [f"**Assistant**: {model_1}\n\n" + f"**<mark><span style='color:black'>Game 1 Judgment</span></mark>**: {g1_judgment[0]}\n\n" + f"**<mark><span style='color:black'>Game 2 Judgment</span></mark>**: {g1_judgment[1]}",
-                f"**Assistant**: {model_2}\n\n" + f"**<mark><span style='color:black'>Game 1 Judgment</span></mark>**: {g2_judgment[0]}\n\n" + f"**<mark><span style='color:black'>Game 2 Judgment</span></mark>**: {g2_judgment[1]}"]
+        return [f"**<mark><span style='color:black'>Game 1 Judgment</span></mark>**: {g1_judgment[0]}\n\n", f"**<mark><span style='color:black'>Game 2 Judgment</span></mark>**: {g1_judgment[1]}"]
     except KeyError:
         return "N/A"
     
@@ -491,45 +478,32 @@ if __name__ == "__main__":
     parser.add_argument("--host", type=str, default="0.0.0.0")
     parser.add_argument("--port", type=int)
     parser.add_argument("--share", action="store_true")
-    parser.add_argument("--bench-name", type=str, default="arena-bench-v1")
-    parser.add_argument("--judge-mode", type=str, required=True)
-    parser.add_argument("--config-file", type=str, default="data/arena-bench-v1_config.yaml")
+    parser.add_argument("--config-file", type=str, default="config/judge_config.yaml")
     args = parser.parse_args()
     print(args)
 
     configs = make_config(args.config_file)
 
-    question_file = f"data/{args.bench_name}/question.jsonl"
-    answer_dir = f"data/{args.bench_name}/model_answer"
+    question_file = f"data/{configs['bench_name']}/question.jsonl"
+    answer_dir = f"data/{configs['bench_name']}/model_answer"
     pairwise_model_judgment_dir = (
-        os.path.join("data", args.bench_name, args.judge_mode)
+        os.path.join("data", configs["bench_name"], "model_judgment", configs["judge_model"])
     )
     single_model_judgment_dir = (
-        os.path.join("data", args.bench_name, args.judge_mode)
+        os.path.join("data", configs["bench_name"], "model_judgment", configs["judge_model"])
     )
-    reference_dir = os.path.join("data", args.bench_name, "reference_answer")
-
     # Load questions
     questions = load_questions(question_file, None, None)
 
     # Load answers
     model_answers = load_model_answers(answer_dir)
 
-    # Load model judgments
-    # model_judgments_normal_single = (
-    #     model_judgments_math_single
-    # ) = load_single_model_judgments(single_model_judgment_dir)
     model_judgments_normal_pairwise = (
         model_judgments_math_pairwise
     ) = load_pairwise_model_judgments(pairwise_model_judgment_dir)
 
-    model_reference_answers = load_reference(reference_dir, configs["ref_model"])
-
     if configs["baseline"]:
         baseline_model = configs["baseline_model"]
-
-    for i in range(len(model_reference_answers)):
-        block_css += f"\n#reference_{i}\n{{\n background-color: #0B0F19;\n}}"
 
     demo = build_demo()
     demo.queue(concurrency_count=10, status_update_rate=10, api_open=False).launch(
